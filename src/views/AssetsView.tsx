0035e90177d65ad672d01d4f2090285159f78fe3
zhuba-Ahhh <3477826311@qq.com>
2025-03-18 14:42:48 +0800

    feat: :sparkles: 优化图表展示

--- src/views/AssetsView.tsx
+++ src/views/AssetsView.tsx
@@ -1,19 +1,10 @@
 import { useEffect, useMemo, useState } from 'react';
-import { Line } from '@ant-design/plots';
+import { Line, LineConfig } from '@ant-design/charts';
 import { AssetData, getLocalAssets } from 'services/info';
-
-const timeGapOptions = [
-  { value: 30, label: '30分钟' },
-  { value: 60, label: '1小时' },
-  { value: 120, label: '2小时' },
-  { value: 240, label: '4小时' },
-  { value: 480, label: '8小时' },
-  { value: 1920, label: '24小时' },
-];
+import dayjs from 'dayjs';
 
 export const AssetsView = () => {
   const [data, setData] = useState<AssetData[]>([]);
-  const [timeGap, setTimeGap] = useState(30); // 默认30分钟
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,12 +21,12 @@ export const AssetsView = () => {
     fetchData();
   }, []);
 
-  const config = useMemo(
+  const config: LineConfig = useMemo(
     () => ({
       data: data.flatMap((item) =>
         item.records.map((record) => ({
           label: item.label,
-          timestamp: new Date(record.timestamp),
+          timestamp: dayjs(record.timestamp).format('MM-DD HH:mm'),
           value: Number(record.data[2]),
         }))
       ),
@@ -48,7 +39,6 @@ export const AssetsView = () => {
         type: 'time',
         tickCount: 5,
         label: {
-          formatter: (v: string) => new Date(v).toLocaleString('zh-CN'),
           style: {
             fill: '#fff',
           },
@@ -64,9 +54,11 @@ export const AssetsView = () => {
       },
       point: {
         size: 4,
+        shape: 'circle',
         style: {
           stroke: '#fff',
           lineWidth: 1,
+          fill: '#1890ff',
         },
       },
       legend: {
@@ -82,25 +74,38 @@ export const AssetsView = () => {
           backgroundColor: 'transparent',
         },
       },
+      tooltip: {},
+      slider: {
+        start: 0,
+        end: 1,
+        height: 20,
+        x: {},
+        y: { labelFormatter: '~s' },
+        trendCfg: {
+          backgroundStyle: {
+            fill: '#363636',
+          },
+        },
+        foregroundStyle: {
+          fill: '#1890ff',
+          fillOpacity: 0.3,
+        },
+        handlerStyle: {
+          fill: '#1890ff',
+          opacity: 1,
+          width: 20,
+          height: 20,
+        },
+        textStyle: {
+          fill: '#fff',
+        },
+      },
     }),
     [data]
   );
 
   return (
-    <div className="p-4 md:p-6 text-white min-h-screen">
-      <div className="mb-4">
-        <select
-          value={timeGap}
-          onChange={(e) => setTimeGap(Number(e.target.value))}
-          className="bg-gray-800 text-white pl-2 pr-1 py-1 rounded-lg border border-gray-700 focus:outline-none focus:border-blue-500"
-        >
-          {timeGapOptions.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.label}
-            </option>
-          ))}
-        </select>
-      </div>
+    <div className="pt-24 md:p-6 text-white min-h-screen flex justify-center">
       <div className="w-full h-[400px]  p-4 rounded-xl shadow-lg">
         {data.length > 0 && <Line {...config} />}
       </div>
